963c0b771e48e91ea7da0ce738f353dd8083dc67
faris <faris.k218@gmail.com>
2024-07-23 17:08:01 +0400

    chore(constants): utils, appUrls, enums, strings

---
+++ lib/core/constants/app_urls.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  static const String url = 'https://api.escuelajs.co/';
+  static const String apiVersion = 'api/v1';
+  static const String baseUrl = '$url$apiVersion';
+  static const String categories = 'categories';
+  static const String products = 'products';
+}
---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,25 @@
+class Assets {
+  //logo
+  static const String logo = 'assets/svg/logo.svg';
+  static const String splashBg = 'assets/svg/BG.svg';
+
+  // tab
+
+  static const String tabBg = 'assets/svg/rectangleBg.svg';
+  static const String homeSelectedIcon = 'assets/svg/bicycle.svg';
+  static const String activitiesSelectedIcon = 'assets/svg/map.fill.svg';
+  static const String cartSelectedIcon = 'assets/svg/cart.fill.svg';
+  static const String ordersSelectedIcon = 'assets/svg/doc.svg';
+  static const String profileSelectedIcon = 'assets/svg/person.fill.svg';
+
+  //banner
+  static const String mainImage = 'assets/svg/main_img.png';
+  static const String mainImageBg = 'assets/svg/main_img_bg.svg';
+  static const String cardBg = 'assets/svg/card_bg.png';
+
+  //Icons
+  static const String search = 'assets/svg/search.svg';
+
+  //Sample
+  static const String placeholderImage = 'https://picsum.photos/200/400';
+}
